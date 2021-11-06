44fe794831644467cd664585154564ef3733d912
Kallaumamahesh <93186608+Kallaumamahesh@users.noreply.github.com>
2021-11-05 23:54:01 -0700

    Add files via upload

--- kalla c.c
+++ kalla c.c
@@ -2,11 +2,19 @@
 #include<stdio.h>
 void main()
 {
-   int a, b, s;
+   int a, b, A, S, M, D, MD;
    a = 10;
    b = 25;
    
-   s = a + b;
+   A = a + b;
+   S = a - b;
+   M = a * b;
+   D=a/b;
+   MD=a%b;
    
-   printf(" sum of %d and %d is %d", a, b, s);	
+   printf(" sum of %d and %d is %d\n", a, b, A);
+   printf(" difference of %d and %d is %d\n", a, b, S);
+   printf("multiplication of %d and %d is %d\n", a, b, M);
+   printf("division of %d and %d is %d\n", a, b, D);
+   printf("remainder of %d & %d is %d", a, b, MD);	
 }
