a7b19ab7152c60f35a66fb7a9dc2fcf406da48c0
Kallaumamahesh <93186608+Kallaumamahesh@users.noreply.github.com>
2021-11-10 22:54:54 -0800

    Add files via upload

--- kallac.c
+++ kallac.c
@@ -1,9 +1,13 @@
+// to find the area and perimeter of a rectangle when side values in given.
 #include<stdio.h>
 void main()
 {
-	int n, formula;
-	n=10;
+	int l, b, a, p;
+	printf("Enter length and width values");
+	scanf("%d%d", &l, &b);
+	a = l*b;
+	p = 2*(l+b);
+	printf("Area of the rectangle is %d\n", a);
+	printf("Perimeter of the rectangle is %d", p);
 	
-	formula=n*(n+1)/2
-	printf(" sum of first %d natural numbers is %d", n, formula);
 }
